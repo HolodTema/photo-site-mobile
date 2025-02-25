17e77631649334bbbe72f19c8ea7b95dc2dc3cf0
Artem Holodilov <art.holodilov@gmail.com>
2025-02-25 13:50:35 +0300

    redirect to desktop version is done.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import App from "./App";
 import {Provider} from "react-redux";
 import store from "./redux/store";
 
+if( ! ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) ) {
+    window.location = "http://videoholod.ru/";
+}
+else {
+    const app = ReactDOMClient.createRoot(document.getElementById('app'))
 
-const app = ReactDOMClient.createRoot(document.getElementById('app'))
-
-app.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>
-)
+    app.render(
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    )
+}
